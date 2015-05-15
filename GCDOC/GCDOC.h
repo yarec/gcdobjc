368fbe107eb297779db417583b578b093b085538
yarec <yarec@163.com>
2015-05-15 12:23:34 +0800

    u

---
+++ GCDOC/GCDOC.h
@@ -0,0 +1,19 @@
+//
+//  GCDOC.h
+//  GCDOC
+//
+//  Created by r.t on 15/5/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GCDOC.
+FOUNDATION_EXPORT double GCDOCVersionNumber;
+
+//! Project version string for GCDOC.
+FOUNDATION_EXPORT const unsigned char GCDOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GCDOC/PublicHeader.h>
+
+
